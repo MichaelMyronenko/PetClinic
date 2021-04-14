8baf8ff4293d4642760e6fac73e2112fab433b92
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-14 10:45:10 +0300

    Task#4/-implement-user-details

---
+++ src/main/java/com/dve/petclinic/security/AuthenticatedUser.java
@@ -0,0 +1,50 @@
+package com.dve.petclinic.security;
+
+
+import com.dve.petclinic.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthenticatedUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.isActive();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive();
+    }
+}
--- src/main/java/com/dve/petclinic/user/UserRepository.java
+++ src/main/java/com/dve/petclinic/user/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<CommonUser, Long> {
+    @EntityGraph("user.roles")
+    Optional<CommonUser> findCommonUserByUsername(String username);
 
     @EntityGraph("user.roles")
     Optional<CommonUser> findById(Long userId);
