cccc5a7f343aaf26dd70c5a59338755c2b243581
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-14 10:42:48 +0300

    Task#3/-add-find-by-id-method-to-user-repository

--- src/main/java/com/dve/petclinic/user/UserRepository.java
+++ src/main/java/com/dve/petclinic/user/UserRepository.java
@@ -1,7 +1,12 @@
 package com.dve.petclinic.user;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<CommonUser, Long> {
 
+    @EntityGraph("user.roles")
+    Optional<CommonUser> findById(Long userId);
 }
