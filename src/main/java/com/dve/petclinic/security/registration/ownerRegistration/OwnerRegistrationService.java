a410d9b58f88cc4fbb45288ccbfef824b4346acd
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-15 10:36:54 +0300

    Task#10/implement-owner-auto-creation-when-user-sign-up

---
+++ src/main/java/com/dve/petclinic/security/registration/ownerRegistration/OwnerRegistrationService.java
@@ -0,0 +1,7 @@
+package com.dve.petclinic.security.registration.ownerRegistration;
+
+import com.dve.petclinic.user.CommonUser;
+
+public interface OwnerRegistrationService {
+    void registerOwner(CommonUser user);
+}
