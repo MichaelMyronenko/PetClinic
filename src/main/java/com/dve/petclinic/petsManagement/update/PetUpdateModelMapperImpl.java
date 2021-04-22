22c76ee52899b7cc2f5964077b7d8db89773ddfe
mmdance <mikhaylo.mironenko.99@gmail.com>
2021-04-22 09:44:37 +0300

    Task#26/refactored-pet-update-model

--- src/main/java/com/dve/petclinic/petsManagement/update/PetUpdateModelMapperImpl.java
+++ src/main/java/com/dve/petclinic/petsManagement/update/PetUpdateModelMapperImpl.java
@@ -4,7 +4,7 @@ import com.dve.petclinic.entities.pet.Pet;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PetUpdateModelMapperImpl implements PetUpdateModelMapper<Pet, PetUpdateModel>{
+public class PetUpdateModelMapperImpl implements PetUpdateModelMapper<Pet, PetUpdateModel> {
     @Override
     public void update(Pet entity, PetUpdateModel model) {
         entity.setName(model.getName());
